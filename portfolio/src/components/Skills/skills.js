160313d461d56daafac710d87df2ef508f22ad8e
neethafrancis <neethafrancis26@gmail.com>
2025-05-14 10:42:21 +0530

    Refactor Intro component and improve image accessibility in Skills component

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import './skills.css';
 import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
 import img from'../../assets/pic4.jpg';
 
 const Skills = () => {
@@ -78,7 +76,7 @@ const Skills = () => {
                     </Accordion>
                 </div>
                 <div >
-                    <img className='image-container' src={img}></img>
+                    <img className='image-container' src={img} alt="img"></img>
                 </div>
             </div>
         </section>
